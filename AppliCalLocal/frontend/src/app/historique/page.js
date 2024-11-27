cc44060c193ad4e25324b62c65d3b46e7de74a90
Zainados2 <TravailTheoGit@outlook.com>
2024-11-27 22:48:27 +0100

    debug

--- AppliCalLocal/frontend/src/app/historique/page.js
+++ AppliCalLocal/frontend/src/app/historique/page.js
@@ -274,6 +274,8 @@ export default function Historique() {
     );
   }
 
+  console.log(filteredEvents)
+
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Historique général</h1>
